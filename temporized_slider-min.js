159ab7acd29fd12cfc72904057928e486ac68ab8
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-01 23:02:05 -0300

    Adding the minified version

---
+++ temporized_slider-min.js
@@ -0,0 +1,8 @@
+/**
+ * TemporizedSlider.js
+ *
+ * Copyright 2012, Lukas Alexandre
+ * Licensed under MIT
+ *
+ * @module temporized_slider
+*/var TemporizedSlider={};TemporizedSlider.init=function(a){var b={default_time:0,data:[{image:"https://www.google.com.br/images/srpr/logo3w.png",title:"my title",text:"temporized text",time:1},{image:"http://railsbrasil.s3.amazonaws.com/sites/4e2c66cde8fb0e0001000004/theme/images/rails.png",title:"my rails title",text:"temporized rails text",time:1}]};for(var c in b)typeof a[c]=="undefined"&&(a[c]=b[c]);collection=a.data,pointer=-1,end=collection.length-1,timeOut=null,paused=!1},TemporizedSlider.play=function(){paused=!1,pointer=pointer+1>end?0:pointer+1,TemporizedSlider.changeContent(),TemporizedSlider.scheduleNextChange()},TemporizedSlider.pause=function(){paused=!0,clearTimeout(timeOut),pointer=pointer-1<0?0:pointer-1},TemporizedSlider.previous=function(){pointer=pointer-1>=0?pointer-1:end,TemporizedSlider.changeContent(),paused||TemporizedSlider.scheduleNextChange()},TemporizedSlider.next=function(){pointer=pointer+1<=end?pointer+1:0,TemporizedSlider.changeContent(),paused||TemporizedSlider.scheduleNextChange()},TemporizedSlider.changeContent=function(){var a=collection[pointer];document.getElementById("slider_image").src=a.image,document.getElementById("slider_title").innerHtml=a.title,document.getElementById("slider_title").textContent=a.title,document.getElementById("slider_text").innerHtml=a.text,document.getElementById("slider_text").textContent=a.text},TemporizedSlider.scheduleNextChange=function(){clearTimeout(timeOut),timeOut=setTimeout("TemporizedSlider.play()",collection[pointer].time*1e3)};
