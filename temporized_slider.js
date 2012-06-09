1e739317a6c6c27eabe3fcf9130d63c32de1591a
Rodrigo Vieira <rodrigovieira1994@gmail.com>
2012-06-09 11:21:04 -0300

    Major refactoring in source code. Removed several global variables and rewrote parts of the code to satisfy JSLint, the JS Code Quality tool.

--- temporized_slider.js
+++ temporized_slider.js
@@ -9,10 +9,12 @@
 
 var TemporizedSlider = {};
 
-TemporizedSlider.init = function(options) {
+TemporizedSlider.init = function (options) {
   args = options;
 
-  if (args.beforeInit != null) args.beforeInit();
+  if (args.beforeInit != null) {
+    args.beforeInit();
+  }
 
   var default_args = {
     default_time : 0,
@@ -26,108 +28,120 @@ TemporizedSlider.init = function(options) {
         previous_id : 'previous_control',
         next_id : 'next_control'
       },
-      functions : {
-        play : function() {
+      fns : {
+        play : function () {
           TemporizedSlider.play();
         },
-        pause : function() {
+        pause : function () {
           TemporizedSlider.pause();
         },
-        previous : function() {
+        previous : function () {
           TemporizedSlider.previous();
         },
-        next : function() {
+        next : function () {
           TemporizedSlider.next();
         }
       }
     }
   };
 
-  for(var index in default_args) {
-    if(typeof args[index] === "undefined") args[index] = default_args[index];
+  for (var index in default_args) {
+    if(typeof args[index] === "undefined") {
+      args[index] = default_args[index];
+
+    }
   }
 
   if (typeof args.data !== "undefined") {
     TemporizedSlider.defineClicks();
 
-    collection = options.data;
-
-    pointer = -1;
-    end = collection.length - 1;
-    timeOut = null;
-    paused = false;
+    var collection = options.data,
+        pointer = -1,
+        end = collection.length - 1,
+        timeOut = null,
+        paused = false;
 
     if (args.afterInit != null) args.afterInit();
     TemporizedSlider.play(true);
   }
 };
 
-TemporizedSlider.play = function(force_play) {
+TemporizedSlider.play = function (force_play) {
   if (paused || force_play) {
     if (args.beforePlay != null) args.beforePlay();
-    paused = false;
-    pointer = (pointer + 1 > end) ? 0 : (pointer + 1);
+    var paused = false,
+        pointer = (pointer + 1 > end) ? 0 : (pointer + 1);
 
     TemporizedSlider.changeContent();
     TemporizedSlider.scheduleNextChange();
   }
 };
 
-TemporizedSlider.pause = function() {
+TemporizedSlider.pause = function () {
   if (!paused) {
     if (args.beforePause != null) args.beforePause();
 
-    paused = true;
+    var paused = true;
 
     clearTimeout(timeOut);
     pointer = (pointer - 1 < 0) ? 0 : (pointer - 1);
   }
 };
 
-TemporizedSlider.previous = function() {
-  pointer = (pointer - 1 >= 0) ? (pointer - 1) : end;
+TemporizedSlider.previous = function () {
+  var pointer = (pointer - 1 >= 0) ? (pointer - 1) : end;
 
   TemporizedSlider.changeContent();
 
-  if(!paused) TemporizedSlider.scheduleNextChange();
+  if (!paused) {
+    TemporizedSlider.scheduleNextChange();
+  }
 };
 
-TemporizedSlider.next = function() {
-  pointer = (pointer + 1 <= end) ? (pointer + 1) : 0;
+TemporizedSlider.next = function () {
+  var pointer = (pointer + 1 <= end) ? (pointer + 1) : 0;
 
   TemporizedSlider.changeContent();
 
-  if(!paused) TemporizedSlider.scheduleNextChange();
+  if (!paused) {
+    TemporizedSlider.scheduleNextChange();
+  }
 };
 
-TemporizedSlider.changeContent = function() {
+TemporizedSlider.changeContent = function () {
   var obj = collection[pointer];
   document.getElementById(args.image_id).src = obj.image;
   document.getElementById(args.title_id).innerHTML = obj.title;
   document.getElementById(args.text_id).innerHTML = obj.text;
-  if (args.afterChange != null) args.afterChange();
+  if (args.afterChange != null) {
+    args.afterChange();
+  }
 };
 
-TemporizedSlider.scheduleNextChange = function() {
+TemporizedSlider.scheduleNextChange = function () {
   clearTimeout(timeOut);
-  timeOut = setTimeout('TemporizedSlider.play(true)', collection[pointer].time * 1000);
+  var timeOut = setTimeout('TemporizedSlider.play(true)', collection[pointer].time * 1000);
 };
 
-TemporizedSlider.defineClicks = function() {
-  var play_control = document.getElementById('play_control');
-  play_control.onclick = function() {
-    args.controls.functions.play();
+TemporizedSlider.defineClicks = function () {
+  var play_control = document.getElementById('play_control'),
+      pause_control = document.getElementById('pause_control'),
+      previous_control = document.getElementById('previous_control'),
+      next_control = document.getElementById('next_control');
+
+  play_control.onclick = function () {
+    args.controls.fns.play();
   };
-  var pause_control = document.getElementById('pause_control');
-  pause_control.onclick = function() {
-    args.controls.functions.pause();
+
+  pause_control.onclick = function () {
+    args.controls.fns.pause();
   };
-  var previous_control = document.getElementById('previous_control');
-  previous_control.onclick = function() {
-    args.controls.functions.previous();
+
+  previous_control.onclick = function () {
+    args.controls.fns.previous();
   };
-  var next_control = document.getElementById('next_control');
-  next_control.onclick = function() {
-    args.controls.functions.next();
+
+  next_control.onclick = function () {
+    args.controls.fns.next();
   };
 };
