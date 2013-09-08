e43eb1be888adf76edb0b67c6fbed44efc369644
Lukas Alexandre <lukas@codelogic.me>
2013-09-07 22:42:31 -0400

    Adding TravisCI with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jasmine: {
+      // Your project's source files
+      src: 'temporized_slider.js',
+      // Your Jasmine spec files
+      options: {
+        specs: 'tests/spec/temporized_slider_spec.js'
+        // Your spec helper files
+        // helpers : 'specs/helpers/*.js'
+      }
+    }
+  });
+
+  // Register tasks.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  // Default task.
+  grunt.registerTask('default', 'jasmine');
+};
